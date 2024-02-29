a1690b0357fbd59a7b83d5f0f6f72adef0e41c51
Gustavo Rost <gustavosrost@outlook.com>
2024-02-29 06:56:51 -0300

    🎉 Initial commit

---
+++ pages/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <login-wrapper></login-wrapper>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginWrapper from '~/components/organisms/LoginWrapper.vue'
+export default defineComponent({
+  components: { LoginWrapper },
+    name: 'LoginView',
+    setup () {
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
